c7dcf63233853e57ff8b1f5e76b8a6153c130e5b
Velpula-Jashwanth <velpulajashwanth1104@gmail.com>
2025-06-18 22:20:09 +0530

    Create next_greater_element.cpp

---
+++ next_greater_element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> map;
+        stack<int> st;
+        for(int num:nums2){
+            while(!st.empty() && num>st.top()){
+                map[st.top()]=num;
+                st.pop();
+            }
+            st.push(num);
+        }
+        while(!st.empty()){
+            map[st.top()]=-1;
+            st.pop();
+        }
+        vector<int> arr;
+        for(int num:nums1){
+            arr.push_back(map[num]);
+        }
+        return arr;
+}
+};
